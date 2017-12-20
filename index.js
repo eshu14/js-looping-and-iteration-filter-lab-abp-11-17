e893e5597dc8b60ccc90b9498211c12390382f5d
Easwari Ramani <eshuprad@gmail.com>
2017-12-20 08:06:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(drivers,name){
+  const val=drivers.filter(function(d){
+  //  let dr=d.toLowerCase();
+    //let nm=name.toLowerCase();
+    if((name.toLowerCase()).indexOf(d.toLowerCase())==0)
+      return name;
+  });
+
+}
+function fuzzyMath(drivers,str){
+
+
+}
+function matchName(drivers,str){
+
+}
